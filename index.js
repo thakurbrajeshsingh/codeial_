4e96ff8b6e075f5262f257b360d3c27167807268
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-07-21 11:10:43 +0530

    Create Controller And Router For Home

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const Port = 8000;
 const app = express();
 
 
+// middle ware
+app.use('/',require('./routes'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Server Running  
 app.listen(Port,(err)=>{
     if(err){
         console.log('Error || Cannot Connect To Express Server');
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+// importing controllers
+const homeController = require('../controllers/home_controller');
+
+
+router.get('/',homeController.home);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
