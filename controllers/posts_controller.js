08d8dcd51566d4db6bc7fbe5ddd1d88111637615
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-07-22 21:01:05 +0530

    Saving Post to db

---
+++ controllers/posts_controller.js
@@ -0,0 +1,13 @@
+const Post = require('../models/post');
+
+
+module.exports.create = function(req,res){
+    Post.create({
+        content:req.body.content,
+        user: req.user._id
+    },function(err,post){
+        if(err){console.log('Error Posting Comment'); return }
+        
+        return res.redirect('back'); 
+    })
+}
