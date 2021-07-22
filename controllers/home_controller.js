767a258d367d00861bde722b2c4664c371d6c822
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-07-23 00:05:59 +0530

    Check Auth on Creating Post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,6 @@
 const Post = require('../models/post');
 
-module.exports.home = function(req,res){
-     
-// Post.find({},function(err,posts){
-//     return res.render('home',{
-//         title:'Home Page',
-//         posts: posts
-//     });
-// })
- 
+module.exports.home = function(req,res){ 
 Post.find({}).populate('user').exec(function (err, posts) {
     return res.render('home',{
         title:'Home Page',
--- models/post.js
+++ models/post.js
@@ -19,3 +19,6 @@ const postSchema = new mongoose.Schema({
 const Post = mongoose.model('Post',postSchema);
 
 module.exports =Post;
+
+
+
