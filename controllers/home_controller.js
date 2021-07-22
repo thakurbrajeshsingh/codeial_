88d5bf7b59de8ef4377203ef3227c936a42ed6e1
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-07-22 21:43:03 +0530

    Display Post And Related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,19 @@
-module.exports.home = (req,res)=>{
-    // console.log(req.cookies);
-    
+const Post = require('../models/post');
+
+module.exports.home = function(req,res){
+     
+// Post.find({},function(err,posts){
+//     return res.render('home',{
+//         title:'Home Page',
+//         posts: posts
+//     });
+// })
+ 
+Post.find({}).populate('user').exec(function (err, posts) {
     return res.render('home',{
-        title:'Home Page'
-    });
+        title:'Home Page',
+        posts: posts
+
+  });
+})
 }
--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 
-const postSchema =new mongoose.Schema({
+const postSchema = new mongoose.Schema({
 
     content:{
         type:String,
