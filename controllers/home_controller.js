bf2457b949f8765f81ecf076b0b1236d01177b5e
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-07-23 23:25:12 +0530

    User Profile Link

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post = require('../models/post');
-
+const User = require('../models/user');
 module.exports.home = function(req, res){
-   
+
     // populate the user of each post
     Post.find({})
     .populate('user')
@@ -12,10 +12,14 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, posts){
-        return res.render('home', {
-            title: 'Home',
-            posts:  posts
-        });
+        User.find({},function(err,users){
+            return res.render('home', {
+                title: 'Home',
+                posts:  posts,
+                all_users:users
+            });
+        })
+
     })
 
 }
