06cf91b1fffc2c47fcc928f45975db2dbe8093d1
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-07-26 00:24:11 +0530

    user null error Handle

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,60 +1,69 @@
-const Post = require('../models/post')
+const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-
-
 module.exports.create = async function(req, res){
-    
-    try {
-        
-    let post = await Post.create({
-        content: req.body.content,
-        user: req.user._id
-    });
-    
-// check if its  xhr request 
-    if(req.xhr){
-        return res.status(200).json({
-            data :{
-                post : post
-            },message:'Post created!'
+    try{
+        let post = await Post.create({
+            content: req.body.content,
+            user: req.user._id
         });
-    }
-
-
-
-    return res.redirect('back');
         
-    } catch (err) {
-        console.log('Error',err);
-        return ;
-    }   
-}
+        if (req.xhr){
+            // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
+            post = await post.populate('user', 'name').execPopulate();
 
-// For deleting post
-module.exports.destroy = async function(req,res){
-    try {
-        let post = await Post.findById(req.params.id)
-    
-        if(post.user == req.user.id){
-            post.remove();
-            await Comment.deleteMany({post:req.params.id});
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
+
+        req.flash('success', 'Post published!');
+        return res.redirect('back');
+
+    }catch(err){
+        req.flash('error', err);
+        // added this to view the error on console as well
+        console.log(err);
+        return res.redirect('back');
+    }
   
-// check if its  xhr request 
-if(req.xhr){
-    return res.status(200).json({
-        data :{
-            post_id : req.params.id,
-        },message:'Post Deleted!'
-    });
 }
 
-            req.flash('success','Posts and comments deleted:')
-                return res.redirect('back')
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.params.id);
+
+        if (post.user == req.user.id){
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id});
+
+
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+            req.flash('success', 'Post and associated comments deleted!');
+
+            return res.redirect('back');
         }else{
+            req.flash('error', 'You cannot delete this post!');
             return res.redirect('back');
         }
-    } catch (err) {
-        console.log('Error',err)
+
+    }catch(err){
+        req.flash('error', err);
+        return res.redirect('back');
     }
+    
 }
