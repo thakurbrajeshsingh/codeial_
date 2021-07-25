e343787714a5e2c57f489e533dd88acbe8a3d06e
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-07-25 13:59:28 +0530

    Creating A Post And Deleting A Data

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,10 +7,22 @@ module.exports.create = async function(req, res){
     
     try {
         
-    await Post.create({
+    let post = await Post.create({
         content: req.body.content,
         user: req.user._id
     });
+    
+// check if its  xhr request 
+    if(req.xhr){
+        return res.status(200).json({
+            data :{
+                post : post
+            },message:'Post created!'
+        });
+    }
+
+
+
     return res.redirect('back');
         
     } catch (err) {
