ecb691b590a688180fe5062cddb37cfabd49169a
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-07-26 10:57:36 +0530

    Showing Avatar On Profile Page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,16 +13,6 @@ module.exports.profile = function (req, res) {
 
 
 module.exports.update = async function (req, res) {
-    // if (req.user.id == req.params.id) {
-    //     User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
-    //         req.flash('success', 'Updated!');
-    //         return res.redirect('back');
-    //     });
-    // } else {
-    //     req.flash('error', 'Unauthorized!');
-    //     return res.status(401).send('Unauthorized');
-    // }
-
     if (req.user.id == req.params.id){
         try {
             let user = await User.findById(req.params.id);
