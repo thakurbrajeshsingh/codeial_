9ad5fa76fed78b026206312f3221498a0397ab14
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-07-29 21:48:06 +0530

    Added NodeMailer

---
+++ mailers/comments_mailer.js
@@ -0,0 +1,17 @@
+const nodeMailer = require('../config/nodemailer');
+
+
+
+exports.newComment = (comment)=>{
+   let htmlString = nodeMailer.renderTemplate({comment:comment},'/comments/new_Comments.ejs');
+
+    nodeMailer.transporter.sendMail({
+    from: 'brajeshkumar7250211016@gmail.com', // sender address
+    to: comment.user.email, // list of receivers
+    subject: 'New Comment Published', // Subject line
+    html: htmlString, // html body
+    },(err,info)=>{
+        if(err){console.log('Error in sending Mail'); return }
+        if(info){console.log('Mail Sent Successfully'); return }
+    })
+}
