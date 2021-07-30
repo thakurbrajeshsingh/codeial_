399cc798ebf3760f0093f2f53cda36b02f6f83af
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-07-30 22:14:59 +0530

    Like Schema

--- models/comment.js
+++ models/comment.js
@@ -14,7 +14,12 @@ const commentSchema = new mongoose.Schema({
     post: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Post'
-    }
+    },
+    Like:[{
+        type:  mongoose.Schema.Types.ObjectId,
+        ref:'Like'
+    }]
+
 },{
     timestamps: true
 });
--- models/post.js
+++ models/post.js
@@ -17,7 +17,11 @@ const postSchema = new mongoose.Schema({
             type:  mongoose.Schema.Types.ObjectId,
             ref: 'Comment'
         }
-    ]
+    ],
+    Like:[{
+        type:  mongoose.Schema.Types.ObjectId,
+        ref:'Like'
+    }]
 },{
     timestamps: true
 });
